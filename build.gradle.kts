79c4da24d64dfbcd767fb6bdcf8a40b1af715106
StageGuard <1355416608@qq.com>
2021-01-15 12:35:10 +0800

    update mirai to 2.0-RC

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ plugins {
     kotlin("plugin.serialization") version "1.4.21"
     id("com.jfrog.bintray") version "1.8.5"
     `maven-publish`
-    id("net.mamoe.mirai-console") version "2.0-M2"
-    id("net.mamoe.kotlin-jvm-blocking-bridge") version "1.5.1"
+    id("net.mamoe.mirai-console") version "2.0-RC"
+    id("net.mamoe.kotlin-jvm-blocking-bridge") version "1.6.0"
 }
 
 group = "org.itxtech"
