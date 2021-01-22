9b64644de79b6bb98db3cd8f4144ed797eb6ef7b
StageGuard <1355416608@qq.com>
2021-01-22 16:01:20 +0800

    add getResourceXXX

--- src/main/kotlin/org/itxtech/miraijs/libs/StorageLib.kt
+++ src/main/kotlin/org/itxtech/miraijs/libs/StorageLib.kt
@@ -1,13 +1,44 @@
 package org.itxtech.miraijs.libs
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import net.mamoe.kjbb.JvmBlockingBridge
 import org.itxtech.miraijs.PluginLib
+import org.itxtech.miraijs.PluginManager
 import org.itxtech.miraijs.PluginScope
 import org.mozilla.javascript.Context
 import org.mozilla.javascript.Scriptable
 import org.mozilla.javascript.ScriptableObject
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.InputStreamReader
+import java.nio.charset.Charset
 
+@Suppress("unused", "MemberVisibilityCanBePrivate")
 class StorageLib(plugin: PluginScope) : PluginLib(plugin) {
     override val nameInJs: String = "storage"
+
+    @JvmBlockingBridge
+    suspend fun getResourceStream(path: String) =
+        withContext(Dispatchers.IO + pluginScope.coroutineContext) {
+            val resFile = getResourceFile(path)
+            resFile.inputStream()
+        }
+
+    @JvmOverloads
+    @JvmBlockingBridge
+    suspend fun getResourceStreamReader(path: String, charset: Charset = Charset.defaultCharset()) =
+        InputStreamReader(getResourceStream(path), charset)
+
+    @JvmOverloads
+    @JvmBlockingBridge
+    suspend fun getResourceString(path: String, charset: Charset = Charset.defaultCharset()) = buildString {
+        getResourceStreamReader(path, charset).readLines().also {
+            append(it)
+        }
+    }
+
+    fun getResourceFile(path: String) = File(pluginScope.dataFolder.absolutePath + path)
 
     @JvmSynthetic
     override fun importTo(scope: Scriptable, context: Context) {
