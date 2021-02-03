0564071e23ad44039a03b3ade432e4ba1cd76916
StageGuard <1355416608@qq.com>
2021-02-04 00:02:45 +0800

    add plugin exception handler & correct coroutine context relationship

--- src/main/kotlin/org/itxtech/miraijs/PluginScope.kt
+++ src/main/kotlin/org/itxtech/miraijs/PluginScope.kt
@@ -2,7 +2,7 @@ package org.itxtech.miraijs
 
 import kotlinx.coroutines.*
 import net.mamoe.mirai.console.command.CommandManager.INSTANCE.unregister
-import net.mamoe.mirai.console.command.RawCommand
+import net.mamoe.mirai.utils.error
 import org.itxtech.miraijs.`package`.PluginPackage
 import org.itxtech.miraijs.libs.JSCommand
 import org.itxtech.miraijs.libs.PrimitivePluginData
@@ -18,12 +18,26 @@ class PluginScope(private val pluginPackage: PluginPackage) : CoroutineScope {
     val id = pluginPackage.config!!.id
     val author = pluginPackage.config!!.author
 
-    private val pluginJob = SupervisorJob()
+    //Parent of all jobs created by plugin
+    val pluginParentJob = Job()
+    //Plugin processing(loading, unloading, etc...)
     override val coroutineContext: CoroutineContext
-        get() = pluginJob + MiraiJs.coroutineContext + dispatcher
+        get() = MiraiJs.coroutineContext + pluginParentJob
 
     @OptIn(ObsoleteCoroutinesApi::class)
     private val dispatcher = newSingleThreadContext(name)
+    
+    val runtimeExceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        MiraiJs.logger.error {
+            "Exception in MiraiJs plugin $name($id) by $author.\nDetail: " +
+            buildString { throwable.run {
+                append(this)
+                append("\n")
+                append(stackTrace.joinToString("\n") { "  at $it" })
+            } }
+        }
+        cancelPluginAndItsChildrenJob()
+    }
 
     private lateinit var ctx: Context
     private lateinit var scope: ImporterTopLevel
@@ -38,7 +52,7 @@ class PluginScope(private val pluginPackage: PluginPackage) : CoroutineScope {
     val data = PrimitivePluginData(name)
 
     @Suppress("BlockingMethodInNonBlockingContext")
-    suspend fun init() = withContext(coroutineContext) { //set propriety
+    suspend fun init() = withContext(dispatcher) { //set propriety
         ctx = Context.enter()
         ctx.optimizationLevel = PluginManager.optimizationLevel
         ctx.languageVersion = Context.VERSION_ES6
@@ -53,51 +67,47 @@ class PluginScope(private val pluginPackage: PluginPackage) : CoroutineScope {
     }
 
     @Suppress("BlockingMethodInNonBlockingContext")
-    suspend fun compileScripts() = withContext(coroutineContext) {
-        try {
-            pluginPackage.consumeScriptReaders {
-                scripts[it] = ctx.compileReader(this, "${name}#$it", 1, null)
-            }
-        } catch (ex: Exception) {
-            unload()
-            throw Context.throwAsScriptRuntimeEx(ex)
+    suspend fun compileScripts() = withContext(dispatcher + runtimeExceptionHandler) {
+        pluginPackage.consumeScriptReaders {
+            scripts[it] =
+                ctx.compileReader(this@consumeScriptReaders, "${name}#$it", 1, null)
         }
     }
 
-    fun load() = launch(coroutineContext) {
-        try {
-            val mainScript = scripts.filterKeys { it == "main" }
-            if (mainScript.count() == 0) {
-                scripts.values.forEach {
-                    it.exec(ctx, scope)
-                }
-            } else {
-                mainScript["main"]?.exec(ctx, scope)
-                scripts.asSequence().filterNot { it.key == "main" }.forEach {
-                    it.value.exec(ctx, scope)
-                }
+    fun load() = launch(dispatcher + runtimeExceptionHandler) {
+        val mainScript = scripts.filterKeys { it == "main" }
+        if (mainScript.count() == 0) {
+            scripts.values.forEach {
+                it.exec(ctx, scope)
             }
-        } catch (ex: Exception) {
-            MiraiJs.logger.error("Plugin occurred an error : ${Context.throwAsScriptRuntimeEx(ex)}")
+        } else {
+            mainScript["main"]?.exec(ctx, scope)
+            scripts.asSequence().filterNot { it.key == "main" }.forEach {
+                it.value.exec(ctx, scope)
+            }
         }
     }
 
-    fun unload() = launch(coroutineContext) {
+    suspend fun unload() = withContext(dispatcher) {
         Context.exit()
         registeredCommands.forEach { it.unregister() }
-        pluginJob.cancelChildren()
-        pluginJob.cancelAndJoin()
+        pluginPackage.closeAndRelease()
+        cancelPluginAndItsChildrenJobAndJoin()
     }
 
-    fun reload() = launch(coroutineContext) {
-        Context.exit()
-        registeredCommands.forEach { it.unregister() }
-        init()
-        compileScripts()
-        load()
+    private suspend fun cancelPluginAndItsChildrenJobAndJoin() {
+        pluginParentJob.runCatching {
+            cancelChildren()
+            cancelAndJoin()
+        }
     }
 
-    override fun toString(): String {
-        return "$name($id)"
+    private fun cancelPluginAndItsChildrenJob() {
+        pluginParentJob.runCatching {
+            cancelChildren()
+            cancel()
+        }
     }
+
+    override fun toString() = "MiraiJs plugin $name($id) by $author"
 }
