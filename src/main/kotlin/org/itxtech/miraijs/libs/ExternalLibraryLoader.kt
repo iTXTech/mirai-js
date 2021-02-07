ba65665575497792c17242b0c6838ceb3399279b
StageGuard <1355416608@qq.com>
2021-02-07 19:14:56 +0800

    init ExternalLibraryLoader

--- src/main/kotlin/org/itxtech/miraijs/libs/ExternalLibraryLoader.kt
+++ src/main/kotlin/org/itxtech/miraijs/libs/ExternalLibraryLoader.kt
@@ -1,20 +1,24 @@
 package org.itxtech.miraijs.libs
 
-import net.mamoe.mirai.utils.*
-import org.itxtech.miraijs.MiraiJs
 import org.itxtech.miraijs.PluginLib
 import org.itxtech.miraijs.PluginScope
 import org.mozilla.javascript.Context
 import org.mozilla.javascript.Scriptable
 import org.mozilla.javascript.ScriptableObject
 
-@Suppress("unused")
-class ExternalJarLoaderLib(plugin: PluginScope) : PluginLib(plugin) {
-    override val nameInJs: String = "external"
 
+class ExternalLibraryLoader(plugin: PluginScope) : PluginLib(plugin) {
+    @JvmSynthetic
+    override val nameInJs: String = "external"
 
     @JvmSynthetic
     override fun importTo(scope: Scriptable, context: Context) {
         ScriptableObject.putProperty(scope, nameInJs, Context.javaToJS(this, scope))
     }
+
+
+}
+
+object ExternalLibraryLoaderImpl {
+
 }
